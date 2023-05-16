df42789bdfb8d8b563343de30c44e483717ef809
Lerry Augusto <lerry.pirani@gmail.com>
2023-05-15 23:09:55 -0300

    DW10

---
+++ lib/src/modules/home/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../../core/ui/helpers/size_extensions.dart';
+import '../../core/ui/styles/colors_app.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: context.colors.secondary,
+      width: context.percentWidth(.5),
+    );
+  }
+}
